3e86794bd08d4a407d1494602b727c21f6db23b7
Alexis Williams <will1397@purdue.edu>
2018-09-12 20:44:25 -0400

    Added test google sign in and google map

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('docker');
 });
+
+app.get('/map', function(req, res) {
+  res.render('map');
+})
+
+app.get('/test', function(req, res) {
+  res.render('test');
+})
 
 app.listen(8081, function() {
   console.log('app started on port 8081');
